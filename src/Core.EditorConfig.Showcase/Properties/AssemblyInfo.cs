a22c3f0ab9ee3298bc129a664622da76350a917f
Stefan Loeffler <s.loeffler@custom-co.de>
2018-09-22 22:35:33 +0200

    switched from AssemblyInfo to Directory.Build.props (msbuild 15.0)

--- src/Core.EditorConfig.Showcase/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Core.EditorConfig.Showcase")]
-[assembly: AssemblyDescription("Showcase for a c# .editorcfg file for usage inside of visual studio.")]
-[assembly: Guid("9502eda1-b34b-4440-a628-8a9e032a6d43")]
